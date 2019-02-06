f70c5140c870d739838953a5336a472706dd9d6b
Brian Aabel <brian.aabel@equifax.com>
2019-02-06 14:04:44 -0500

    Updated names and packages

--- src/main/java/com/equifax/syntheticid/Application.java
+++ src/main/java/com/equifax/syntheticid/Application.java
@@ -1,4 +1,4 @@
-package com.equifax.sidfrontend;
+package com.equifax.syntheticid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,15 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class SidFrontEndApplication {
+public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World Version 2";
+        return "Hello Docker World Version";
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(SidFrontEndApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
--- src/test/java/com/equifax/syntheticid/ApplicationTests.java
+++ src/test/java/com/equifax/syntheticid/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.equifax.sidfrontend;
+package com.equifax.syntheticid;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SidFrontEndApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
